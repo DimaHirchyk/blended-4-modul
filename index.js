85affac34d4bb07c387e924607dd4e6a4443c09a
Dima Hirchyk <dimzuk04@gmail.com>
2024-12-21 12:00:08 +0200

    first

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //index
+
+// task 1
+/* const input = document.querySelector("#input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  input.value = "Привіт";
+});
+*/
+
+// task 2
+
+const btn = document.querySelector("button");
+const input = document.querySelector("#input");
+
+btn.addEventListener("click", () => {
+  alert(input.value);
+});
